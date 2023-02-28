a103864536e4eb6e5db27a18f1e11129b00d3080
shubhamnimje77@gmail.com <shubhamnimje77@gmail.com>
2023-03-01 04:19:13 +0530

    extra button added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Pageinfo from './Pageinfo'
-import Textpreview from './Textpreview'
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react'
 
@@ -16,13 +15,46 @@ const Body = () => {
         settext(newtext);
         console.log('clicked on button'); 
     }
+
+    const ConvertToLowerCase = () => {
+      let newtext = text.toLowerCase();
+        settext(newtext);
+        console.log('clicked on button'); 
+    }
+
   return (
     <div className='container flex justify-content-center items-center'>
       <div className='row'>
       <Pageinfo />
-        <div className='col-md-8'>
+        <div className='col-md-9'>
           <div className='row'>
           <form className='container'>
+          <div className='row'>
+          <div className='col'>
+          <Button variant="primary" className='container my-2'>Bold</Button>
+          </div>
+          <div className='col'>
+          <Button variant="secondary" className='container my-2'>Italic</Button>
+          </div>
+          <div className='col'>
+          <Button variant="success" className='container my-2'>H1</Button>
+          </div>
+          <div className='col'>
+          <Button variant="danger" className='container my-2'>H2</Button>
+          </div>
+          <div className='col'>
+          <Button variant="warning" className='container my-2'>H3</Button>
+          </div>
+          <div className='col'>
+          <Button variant="info" onClick={ConvertToUpperCase} className='container my-2'>Uppercase</Button>
+          </div>
+          <div className='col'>
+          <Button variant="dark" onClick={ConvertToLowerCase} className='container my-2'>Lowercase</Button>
+          </div>
+          <div className='col'>
+          <Button variant="success" onClick={ConvertToLowerCase} className='container my-2'>Copy</Button>
+          </div>
+          </div>
         <textarea className="form-control hidden-resize h-20"
         type='text'
         size="lg"
@@ -32,13 +64,15 @@ const Body = () => {
         </textarea>
     </form>
           </div>
-          <div className='row'>
-              <Textpreview />
-          </div>
         </div>
-        <div className='col text-center'>
-          <p>Convert To:</p>
-          <Button variant="primary" onClick={ConvertToUpperCase}>Uppercase</Button>
+        <div className='col bg-light p-3'>
+          <h5>Text Summary :</h5>
+          <p>Reading time : <span className='fw-bold'>{0.008 * text.split(' ').length} Minutes</span></p>
+          <p>Letters : <span className='fw-bold'>{text.split(" ").join("").length}</span></p>
+          <p>Characters : <span className='fw-bold'>{text.length}</span></p>
+          <p>Words : <span className='fw-bold'>{text.split(' ').length-1}</span></p>
+          <p>Sentences : <span className='fw-bold'>{text.split('.').length-1}</span></p>
+          <p>Paragraphs : <span className='fw-bold'>{text.split(/\r?\n/).length-1}</span></p>
         </div>
       </div>      
     </div>
--- src/components/Pageinfo.js
+++ src/components/Pageinfo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Pageinfo = () => {
   return (
-    <div className='contaner text-center py-2'>
+    <div className='contaner text-center my-5 bg-secondary text-white'>
       <h1 className='py-3'>Your One-Stop Shop for Text Manipulation</h1>
       <p>TextUtils is a powerful online tool for manipulating
         and formatting text. With a wide range of features,
